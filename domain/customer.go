9f12d104187585d6c5c44ddac1ba346eaf52a4e9
Jay Toy <jay_toyjianying@hotmail.com>
2022-05-27 19:43:32 +0200

    update customer domain

--- domain/customer.go
+++ domain/customer.go
@@ -3,9 +3,13 @@ package domain
 import "time"
 
 type Customer struct {
-	ID        uint      `json:"id"`
+	ID        int       `json:"id" gorm:"primary_key"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (c *Customer) TableName() string {
+	return "customer"
 }
