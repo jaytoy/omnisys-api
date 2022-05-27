3056bf55564914e4424339c8767f587066a024ad
Jay Toy <jay_toyjianying@hotmail.com>
2022-05-27 16:53:21 +0200

    edit customer model

--- domain/customer.go
+++ domain/customer.go
@@ -1,7 +1,11 @@
 package domain
 
+import "time"
+
 type Customer struct {
-	ID    uint   `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
