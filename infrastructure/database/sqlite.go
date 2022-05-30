c60eeb5989812d4d66d0f539e907c53ceb2326dc
Jay Toy <jay_toyjianying@hotmail.com>
2022-05-30 19:48:45 +0200

    add database

---
+++ infrastructure/database/sqlite.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"omnisys.io/core/domain"
+)
+
+func ConnectDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("omnisys.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(domain.Product{})
+
+	return db
+}
