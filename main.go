d8f67951164535481b8a4cedbba1dd07514912e2
Jay Toy <jay_toyjianying@hotmail.com>
2022-05-30 20:15:34 +0200

    Revert "register all services"
    This reverts commit 3e64dba84c418c98cd1b6a7e556d8b018cbd5094.

--- main.go
+++ main.go
@@ -1,25 +1,4 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"omnisys.io/core/application"
-	"omnisys.io/core/infrastructure/database"
-	"omnisys.io/core/interfaces/controller"
-	"omnisys.io/core/interfaces/repository"
-)
-
 func main() {
-	// register the database
-	db := database.ConnectDB()
-
-	// register the router
-	router := gin.Default()
-
-	// register customer module
-	customerRepo := repository.CreateCustomerRepository(db)
-	customerApp := application.CreateCustomerApp(customerRepo)
-	controller.CreateCustomerController(router, customerApp)
-
-	// start server
-	router.Run("localhost:8080")
 }
